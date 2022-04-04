30b524f3bb7dc07880295c04ca9ce61889679a94
dmknapp2385 <dmknapp2385@gmail.com>
2022-04-04 10:38:14 -0700

    navbar basic setup, needs login/signupform still

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -1,32 +1,43 @@
 import React from "react";
-// import {Nav} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Container, Nav, Navbar as RNavbar } from 'react-bootstrap';
+
 // import Auth from '../../utils/auth';
 
 function Navbar(props) {
     return (
-        <nav>
-            <h1>
-                Rocks and Ropes
-            </h1>
-            <div>
-                <ul>
-                    <li>Home</li>
-                    <li>Weights</li>
-                    <li>Upstairs</li>
-                    <li>Login</li>
-                    {/* {Auth.loggedIn() ? (
-                        <>
-                        <Nav.Link as={Link} to='/calendar'>
-                            Calendar
-                        </Nav.Link>
-                        <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
-                        </>
-                    ) : (
-                        <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
-                    )} */}
-                </ul>
-            </div>
-        </nav>
+        <RNavbar bg='dark' variant='dark' expand='lg'>
+        <Container fluid>
+          <RNavbar.Brand>
+            Tour of Rocks and Ropes
+          </RNavbar.Brand>
+          <RNavbar.Toggle aria-controls='navbar' />
+          <RNavbar.Collapse id='navbar'>
+            <Nav className='ml-auto'>
+              <Nav.Link href="/">
+                Home
+              </Nav.Link>
+              <Nav.Link href="/upstairs">
+                Upstairs
+              </Nav.Link>
+              <Nav.Link href="/weights">
+                Weights
+              </Nav.Link>
+              {/* if user is logged in show saved books and logout */}
+              {/* {Auth.loggedIn() ? (
+                <>
+                  <Nav.Link as={Link} to='/calendar'>
+                    See Your Exercises
+                  </Nav.Link>
+                  <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
+                </>
+              ) : (
+                <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
+              )} */}
+            </Nav>
+          </RNavbar.Collapse>
+        </Container>
+      </RNavbar>
     )
 }
 
--- client/src/Pages/Weights.js
+++ client/src/Pages/Weights.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Container, Row, Col } from 'react-bootstrap';
 
 function Weights () {
     return(
         <div>
-            Weights Page
+            <h1>Free Weights</h1>
+            <Container>
+                <Row>
+                    <Col md={{span:4}}>Image of Freeweights</Col>
+                    <Col md={{span:4, offset:3}}>Image of Rings</Col>
+                </Row>
+                <Row>
+                    <Col md={{span:4, offset:4}}>Image of TRX</Col>
+                </Row>
+            </Container>
         </div>
     )
 }
