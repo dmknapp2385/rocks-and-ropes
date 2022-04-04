47112bc50931bde15eaca815d7717f1543e7f78e
Cadenza <mirandalucsb@gmail.com>
2022-04-04 11:58:30 -0700

    added login and signup pages, made sure server could start

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -26,8 +28,10 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <>
-        </>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+        </Switch>
       </Router>
     </ApolloProvider>
   );
