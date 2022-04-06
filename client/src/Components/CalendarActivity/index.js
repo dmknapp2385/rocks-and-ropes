da4f358161ca224e982dfe2ede76eed2e7b31b26
Cadenza <mirandalucsb@gmail.com>
2022-04-06 16:20:32 -0700

    added detail modal to calendar

--- client/src/Components/CalendarActivity/index.js
+++ client/src/Components/CalendarActivity/index.js
@@ -1,20 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashCan, faPlus } from '@fortawesome/free-solid-svg-icons';
-
+import DetailModal from '../DetailModal';
 const CalendarActivity = ({ activity }) => {
-
-
+    const [showDetailModal , setShowDetailModal] = useState(false);
     const handleDeleteClick = (event) => {
         //delete activity call here
     }
 
     const handleDetailClick = (event) => {
         //open up detail modal here
+        setShowDetailModal(true);
     }
 
     return (
         <div className="border border-dark m-2 text-center">
+            <DetailModal showDetailModal = {showDetailModal} setShowDetailModal = {setShowDetailModal} activity = {activity}/>
             <div>
                 <p className="font-weight-bold">{activity.name}</p>
             </div>
