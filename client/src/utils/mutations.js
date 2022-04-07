50819c3f82e6810ffc5c0c22c4818d09e60bdca2
sm3131 <sm2683@nau.edu>
2022-04-06 19:45:18 -0700

    added more mutations and fixed remove all activities resolver

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,19 @@
 import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+{
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+          token
+            user {
+            _id
+            username
+            email
+          }
+        }
+    }
+}`
+
 export const LOGIN_USER = gql`
 {
     mutation Login($email: String!, $password: String!) {
@@ -15,3 +30,46 @@ export const LOGIN_USER = gql`
         }
       }
 }`
+
+export const ADD_ACTIVITY = gql`
+{
+    mutation AddActivity($input: ActivityInput) {
+        addActivity(input: $input) {
+          username
+          email
+          savedActivities {
+            _id
+            day
+            name
+            note
+            length
+            link
+            sets
+            reps
+          }
+        }
+      }
+}`
+
+export const REMOVE_ACTIVITY = gql`
+{
+    mutation RemoveActivity($activityId: ID!) {
+        removeActivity(activityId: $activityId) {
+          _id
+          username
+          email
+          savedActivities {
+            _id
+            day
+            length
+            name
+            note
+            sets
+            reps
+            link
+          }
+        }
+    }
+}`
+
+
