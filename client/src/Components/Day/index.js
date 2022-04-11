90dedf85177f9045886cbf55f57e06bf683f67e3
Cadenza <mirandalucsb@gmail.com>
2022-04-11 15:55:16 -0700

    added extra styling to calendar

--- client/src/Components/Day/index.js
+++ client/src/Components/Day/index.js
@@ -5,8 +5,8 @@ const Day = (props) => {
     const activities = props?.activities || [];
  return (
     <div className="d-flex flex-column col-12 border border-dark background-brown">
-        <p className = "border border-dark rounded mt-2 background-tan shadow" style = {{fontSize: "1.5em"}}>{day}</p>
-        <div className="d-flex flex-xs-row flex-md-column" >
+        <p className = "border border-dark rounded mt-2 background-red text-tan shadow" style = {{fontSize: "1.5em"}}>{day}</p>
+        <div className="d-flex flex-row justify-content-center flex-wrap" >
             {activities.map((activity, i)  => {
                 return <CalendarActivity activity = {activity} days = {days} activities = {allActivities} setActivities={setActivities} detailModalProps={detailModalProps} key={`${day}${activity.name}${i}`}/>
             })}
