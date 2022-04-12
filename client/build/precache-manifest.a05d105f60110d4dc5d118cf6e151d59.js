09795f94e9495073391e98a3a15094173c94e9fa
Michael Walker <michaelwwalker42@gmail.com>
2022-04-11 20:12:28 -0700

    ran build

---
+++ client/build/precache-manifest.a05d105f60110d4dc5d118cf6e151d59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dadd63d07b28b921cd4f490989f5c14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcbc1fa889dad0f842a5",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "981155f4bbf2015d6afb",
+    "url": "/static/css/main.2bd4c4a3.chunk.css"
+  },
+  {
+    "revision": "fcbc1fa889dad0f842a5",
+    "url": "/static/js/2.118fd3a3.chunk.js"
+  },
+  {
+    "revision": "1cd4b25c3d20bf9078255bf51343c01c",
+    "url": "/static/js/2.118fd3a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "981155f4bbf2015d6afb",
+    "url": "/static/js/main.162d4432.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
